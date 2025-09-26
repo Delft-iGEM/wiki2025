42638d098749ef0ae42405685d13d8880b50038b
Franek <fkolinski@tudelft.nl>
2025-09-26 16:53:45 +0200

    Update MembersGrid and Home page

--- src/contents/home.tsx
+++ src/contents/home.tsx
@@ -7,6 +7,7 @@ export function Home() {
       <AccentLine />
       <span className="text-5xl font-bold">snaccine </span>
 
+      {/* Interactive schematic */}
       <div id="schmcont" className="flex flex-col items-center justify-center my-8">
         <div className="h-250">
           <InteractiveSchematic />
@@ -14,22 +15,33 @@ export function Home() {
       </div>
 
       {/* Images section */}
-      <div className="flex flex-wrap justify-center gap-6 my-12">
-        <img
-          src="https://static.igem.wiki/teams/5649/roundicons/illustration-sans-titre-5.webp"
-          alt="iGEM Illustration 1"
-          className="w-40 h-40 object-contain"
-        />
-        <img
-          src="https://static.igem.wiki/teams/5649/roundicons/illustration-sans-titre-4-1.webp"
-          alt="iGEM Illustration 2"
-          className="w-40 h-40 object-contain"
-        />
-        <img
-          src="https://static.igem.wiki/teams/5649/roundicons/illustration-sans-titre-2.webp"
-          alt="iGEM Illustration 3"
-          className="w-40 h-40 object-contain"
-        />
+      <div className="flex flex-wrap justify-center items-start gap-6 my-12">
+        {/* Human Practices */}
+        <div className="w-50 h-50 flex items-center justify-center overflow-hidden">
+          <img
+            src="https://static.igem.wiki/teams/5649/roundicons/human-practices-cb.webp"
+            alt="Human Practices Illustration"
+            className="max-w-full max-h-full object-contain"
+          />
+        </div>
+
+        {/* Engineering */}
+        <div className="w-50 h-50 flex items-center justify-center overflow-hidden -mt-2 rotate-3">
+          <img
+            src="https://static.igem.wiki/teams/5649/roundicons/engineering-cb.webp"
+            alt="Engineering Illustration"
+            className="max-w-full max-h-full object-contain"
+          />
+        </div>
+
+        {/* Team */}
+        <div className="w-50 h-50 flex items-center justify-center overflow-hidden rotate-6">
+          <img
+            src="https://static.igem.wiki/teams/5649/roundicons/team-clear-background.webp"
+            alt="Team Illustration"
+            className="max-w-full max-h-full object-contain"
+          />
+        </div>
       </div>
     </div>
   );
