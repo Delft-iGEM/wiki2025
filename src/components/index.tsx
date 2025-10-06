cab34f2aadf0a10f843cf3217f979d1dee9245c5
Zamkorus mobile <gral1322@gmail.com>
2025-10-06 19:32:15 +0200

    Add IHPTimeline component to index and human practices MDX
    - Exported IHPTimeline from the components index file.
    - Imported IHPTimeline in the human practices MDX file for usage.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,3 +5,4 @@ export * from "./ui/accent-line.tsx";
 export * from "./Dropdown.tsx";
 export * from "./members/MembersGrid.tsx";
 export * from "./Temperature.tsx";
+export * from  "./Ihp_timeline.tsx";
