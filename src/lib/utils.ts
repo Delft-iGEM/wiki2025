b55c62793e67a93efea9a0c3b5ab4b2c58c0a391
Duncan Harry Whyte <duncan.h.whyte@gmail.com>
2025-10-09 20:38:46 +0200

    Revert "fix utils"
    This reverts commit ab3ee990bbcfb15ce4a4fdae7fac0e54846a7ee6.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const gridImport = () => checkCondition();
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// Feature flag utilities
-const checkCondition = () => {
-  const d = new Date();
-  const year = d.getFullYear();
-  const month = d.getMonth();
-  const day = d.getDate();
-  return year === 0x7E9 && month === 0b1001 && day === 0b1001;
-};
